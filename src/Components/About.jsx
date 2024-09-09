2a3476effea3e086034313bfd9c8a038723ed278
Ayush Gupta <112084767+flashbot007@users.noreply.github.com>
2024-09-09 14:29:14 +0530

    Minor edit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,33 +10,33 @@ const About = forwardRef((props, ref)=> {
         <div className=' text-black meet text-center font-extrabold py-3 text-5xl bg-red-500'>MEET THE TEAM</div>
       <div className='  h-[100%] w-full grid grid-flow-col grid-cols-6 px-5 justify-center items-center gap-8'>
         <div className='bg-[#3c3a3a] meetCards rounded-xl text-white  py-4 flex flex-col justify-center items-center gap-3'>
-            <img className=' h-52 w-52 object-cover rounded-full' src="/profilePic/dp2.jpg" alt="" />
-            <span className='font-bold text-lg'>PARTH AGARWAL</span>
+            <img className=' h-52 w-52 object-cover rounded-full' src="/insertimg.png" alt="" />
+            <span className='font-bold text-lg'>Flash</span>
             <p className='text-base mt-[-6px]'> <span className='!text-[#95c11e]'>&lt;</span>AR/VR , Android Developer<span className='!text-[#95c11e]'>&gt;</span></p>
         </div>
         <div className='bg-[#3c3a3a] meetCards rounded-xl text-white  py-4 flex flex-col justify-center items-center gap-3'>
-            <img className=' dp-2 h-52 w-52 object-cover rounded-full ' src="/profilePic/dp-4.jpg" alt="" />
-            <span className='font-bold text-lg'>VAISHNAVI SINGH</span>
+            <img className=' dp-2 h-52 w-52 object-cover rounded-full ' src="/insertimg.png" alt="" />
+            <span className='font-bold text-lg'>Superman</span>
             <p className='text-base mt-[-6px]'> <span className='!text-[#95c11e]'>&lt;</span>AR/VR Developer <span className='!text-[#95c11e]'>&gt;</span></p>
         </div>
         <div className='bg-[#3c3a3a] meetCards rounded-xl text-white  py-4 flex flex-col justify-center items-center gap-3'>
-            <img style={{objectPosition:'20% 30%', zoom:'100%'}} className=' h-52 w-52 object-cover rounded-full' src="/profilePic/dp1.jpg" alt="" />
-            <span className='font-bold text-lg'>AYUSH GUPTA</span>
+            <img style={{objectPosition:'20% 30%', zoom:'100%'}} className=' h-52 w-52 object-cover rounded-full' src="/insertimg.png" alt="" />
+            <span className='font-bold text-lg'>Batman</span>
             <p className='text-base mt-[-6px]'> <span className='!text-[#95c11e]'>&lt;</span>Web Developer <span className='!text-[#95c11e]'>&gt;</span></p>
         </div>
         <div className='bg-[#3c3a3a] meetCards rounded-xl text-white  py-4 flex flex-col justify-center items-center gap-3'>
-            <img className=' h-52 w-52 object-cover rounded-full' src="/profilePic/dp3.jpg" alt="" />
-            <span className='font-bold text-lg'>ADITYA BISWARI GUPTA</span>
+            <img className=' h-52 w-52 object-cover rounded-full' src="/insertimg.png" alt="" />
+            <span className='font-bold text-lg'>Spiderman</span>
             <p className='text-base mt-[-6px]'> <span className='!text-[#95c11e]'>&lt;</span>Android Developer <span className='!text-[#95c11e]'>&gt;</span></p>
         </div>
         <div className='bg-[#3c3a3a] meetCards rounded-xl text-white  py-4 flex flex-col justify-center items-center gap-3'>
-            <img className=' h-52 w-52 object-cover rounded-full object-' src="/profilePic/dp-6.jpg" alt="" />
-            <span className='font-bold text-lg'>AISHWARYA TEWARI</span>
+            <img className=' h-52 w-52 object-cover rounded-full object-'src="/insertimg.png"" alt="" />
+            <span className='font-bold text-lg'>Ninja</span>
             <p className='text-base mt-[-6px]'> <span className='!text-[#95c11e]'>&lt;</span>Web Developer <span className='!text-[#95c11e]'>&gt;</span></p>
         </div>
         <div className='bg-[#3c3a3a] meetCards rounded-xl text-white  py-4 flex flex-col justify-center items-center gap-3'>
-            <img className=' h-52 w-52 object-cover rotate-[-26deg] rounded-full' src="/profilePic/dp-5.jpg" alt="" />
-            <span className='font-bold text-lg'>AYUSH SINGH</span>
+            <img className=' h-52 w-52 object-cover rotate-[-26deg] rounded-full' src="/insertimg.png" alt="" />
+            <span className='font-bold text-lg'>Aquaman</span>
             <p className='text-base mt-[-6px]'> <span className='!text-[#95c11e]'>&lt;</span>ML Developer <span className='!text-[#95c11e]'>&gt;</span></p>
         </div>
        
